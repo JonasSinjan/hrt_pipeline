ecca8e85fe0d267b0e16efe5f736667dace3583f
JonasSinjan <einsteinisbae@gmail.com>
2021-09-24 19:46:10 +0200

    refactor, cleaning up hrt_pipe, moving more funcs to hrt_pipe_sub - less magic numbers

--- hrt_pipe_sub.py
+++ hrt_pipe_sub.py
@@ -7,7 +7,97 @@ import os
 import time
 import subprocess
 
-def demod_hrt(data,pmp_temp):
+def load_dark(dark_f) -> np.ndarray:
+    """
+    loads dark field from given path
+    """
+    print(" ")
+    printc('-->>>>>>> Reading Darks',color=bcolors.OKGREEN)
+
+    start_time = time.time()
+
+    try:
+        dark,_ = get_data(dark_f)
+
+        dark_shape = dark.shape
+
+        if dark_shape != (2048,2048):
+            
+            printc("Dark Field Input File not in 2048,2048 format: {}",dark_f,color=bcolors.WARNING)
+            printc("Attempting to correct ",color=bcolors.WARNING)
+
+            
+            try:
+                if dark_shape[0] > 2048:
+                    dark = dark[dark_shape[0]-2048:,:]
+            
+            except Exception:
+                printc("ERROR, Unable to correct shape of dark field data: {}",dark_f,color=bcolors.FAIL)
+
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
+        printc(f"------------ Load darks time: {np.round(time.time() - start_time,3)} seconds",bcolors.OKGREEN)
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
+
+        return dark
+
+    except Exception:
+        printc("ERROR, Unable to open darks file: {}",dark_f,color=bcolors.FAIL)
+
+
+def apply_dark_correction(data, flat, dark, header_imgdirx_exists, imgdirx_flipped, rows, cols) -> np.ndarray:
+    """
+    subtracts dark field from flat field and science data
+    """
+    print(" ")
+    print("-->>>>>>> Subtracting dark field")
+    
+    start_time = time.time()
+
+    if header_imgdirx_exists:
+        if imgdirx_flipped == 'YES':
+            dark_copy = np.copy(dark)
+            dark_copy = dark_copy[:,::-1]
+
+            data -= dark_copy[rows,cols, np.newaxis, np.newaxis, np.newaxis] 
+            flat -= dark_copy[..., np.newaxis, np.newaxis]
+            
+        elif imgdirx_flipped == 'NO':
+            data -= dark[rows,cols, np.newaxis, np.newaxis, np.newaxis]
+            flat -= dark[..., np.newaxis, np.newaxis]
+    else:
+        data -= dark[rows,cols, np.newaxis, np.newaxis, np.newaxis] 
+        flat -= dark[..., np.newaxis, np.newaxis]
+    printc('--------------------------------------------------------------',bcolors.OKGREEN)
+    printc(f"------------- Dark Field correction time: {np.round(time.time() - start_time,3)} seconds",bcolors.OKGREEN)
+    printc('--------------------------------------------------------------',bcolors.OKGREEN)
+
+    return data, flat
+
+
+def normalise_flat(flat, flat_f, ceny, cenx) -> np.ndarray:
+    """
+    normalise flat fields at each wavelength position to remove the spectral line
+    """
+    print(" ")
+    printc('-->>>>>>> Normalising Flats',color=bcolors.OKGREEN)
+
+    start_time = time.time()
+
+    try:
+        norm_fac = np.mean(flat[ceny,cenx, :, :], axis = (0,1))[np.newaxis, np.newaxis, ...]  #mean of the central 1k x 1k
+        flat /= norm_fac
+
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
+        printc(f"------------- Normalising flat time: {np.round(time.time() - start_time,3)} seconds",bcolors.OKGREEN)
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
+        
+        return flat
+
+    except Exception:
+        printc("ERROR, Unable to normalise the flat fields: {}",flat_f,color=bcolors.FAIL)
+
+
+def demod_hrt(data,pmp_temp) -> np.ndarray:
     '''
     Use constant demodulation matrices to demodulate input data
     '''
@@ -95,35 +185,52 @@ def unsharp_masking(flat,sigma,flat_pmp_temp,cpos_arr,clean_mode,pol_end=4):
     return np.matmul(invM, new_demod_flats*norm_factor)
 
 
-def flat_correction(data,flat,flat_states,rows,cols):
+def flat_correction(data,flat,flat_states,rows,cols) -> np.ndarray:
     """
     correct science data with flat fields
     """
-    if flat_states == 6:
-        
-        printc("Dividing by 6 flats, one for each wavelength",color=bcolors.OKGREEN)
+    print(" ")
+    printc('-->>>>>>> Correcting Flatfield',color=bcolors.OKGREEN)
+
+    start_time = time.time()
+
+    try: 
+        if flat_states == 6:
             
-        tmp = np.mean(flat,axis=-2) #avg over pol states for the wavelength
+            printc("Dividing by 6 flats, one for each wavelength",color=bcolors.OKGREEN)
+                
+            tmp = np.mean(flat,axis=-2) #avg over pol states for the wavelength
 
-        return data / tmp[rows,cols, np.newaxis, :, np.newaxis]
+            return data / tmp[rows,cols, np.newaxis, :, np.newaxis]
 
 
-    elif flat_states == 24:
+        elif flat_states == 24:
 
-        printc("Dividing by 24 flats, one for each image",color=bcolors.OKGREEN)
+            printc("Dividing by 24 flats, one for each image",color=bcolors.OKGREEN)
 
-        return data / flat[rows,cols, :, :, np.newaxis] #only one new axis for the scans
+            return data / flat[rows,cols, :, :, np.newaxis] #only one new axis for the scans
+                
+        elif flat_states == 4:
+
+            printc("Dividing by 4 flats, one for each pol state",color=bcolors.OKGREEN)
+
+            tmp = np.mean(flat,axis=-1) #avg over wavelength
+
+            return data / tmp[rows,cols, :, np.newaxis, np.newaxis]
+        else:
+            print(" ")
+            printc('-->>>>>>> Unable to apply flat correction. Please insert valid flat_states',color=bcolors.WARNING)
+
             
-    elif flat_states == 4:
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
+        printc(f"------------- Flat Field correction time: {np.round(time.time() - start_time,3)} seconds ",bcolors.OKGREEN)
+        printc('--------------------------------------------------------------',bcolors.OKGREEN)
 
-        printc("Dividing by 4 flats, one for each pol state",color=bcolors.OKGREEN)
+        return data
 
-        tmp = np.mean(flat,axis=-1) #avg over wavelength
+    except: 
+        printc("ERROR, Unable to apply flat fields",color=bcolors.FAIL)
 
-        return data / tmp[rows,cols, :, np.newaxis, np.newaxis]
-    else:
-        print(" ")
-        printc('-->>>>>>> Unable to apply flat correction. Please insert valid flat_states',color=bcolors.WARNING)
 
 
 def prefilter_correction(data,voltagesData_arr,prefilter,prefilter_voltages):
@@ -161,6 +268,32 @@ def prefilter_correction(data,voltagesData_arr,prefilter,prefilter_voltages):
             data[:,:,:,wv,scan] /= imprefilter[...,np.newaxis]
             
     return data
+
+
+def apply_field_stop(data, rows, cols, header_imgdirx_exists, imgdirx_flipped) -> np.ndarray:
+    """
+    apply field stop mask to the science data
+    """
+    print(" ")
+    printc("-->>>>>>> Applying field stop",color=bcolors.OKGREEN)
+
+    start_time = time.time()
+    
+    field_stop,_ = load_fits('./field_stop/HRT_field_stop.fits')
+
+    field_stop = np.where(field_stop > 0,1,0)
+
+    if header_imgdirx_exists:
+        if imgdirx_flipped == 'YES': #should be YES for any L1 data, but mistake in processing software
+            field_stop = field_stop[:,::-1] #also need to flip the flat data after dark correction
+
+    data *= field_stop[rows,cols,np.newaxis, np.newaxis, np.newaxis]
+
+    printc('--------------------------------------------------------------',bcolors.OKGREEN)
+    printc(f"------------- Field stop time: {np.round(time.time() - start_time,3)} seconds",bcolors.OKGREEN)
+    printc('--------------------------------------------------------------',bcolors.OKGREEN)
+
+    return data, field_stop
 
 def CT_ItoQUV(data, ctalk_params, norm_stokes, cpos_arr):
     """
