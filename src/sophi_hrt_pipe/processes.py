288a11d0eb41129f4117f753752051d77bbc6769
daniele calchetti <daniele.calchetti@gmail.com>
2025-01-30 10:55:23 +0100

    small bug fix

--- src/sophi_hrt_pipe/processes.py
+++ src/sophi_hrt_pipe/processes.py
@@ -1771,7 +1771,7 @@ def wavelength_registration(data, cpos_arr, sly, slx, hdr_arr, derivative = True
             # old_data, _ = fran_restore(dat, datetime.datetime.fromisoformat(hdr_arr[scan]['DATE-OBS']),
             #                             mask=np.ones((dat.shape[0],dat.shape[1])), gamma2=0.02, low_f=0.8, aberr_cor=False)
             old_data, _ = fran_restore(dat, datetime.datetime.fromisoformat(hdr_arr[scan]['DATE-OBS']), sly=slice(0,dat.shape[0]), slx=slice(0,dat.shape[1]),
-                                        mask=np.ones((dat.shape[0],dat.shape[1])), gamma2=0, low_f=0.1, aberr_cor=False, PD_f=deconv['PD_f'], straylight_corr=deconv['straylight_corretion'])
+                                        mask=np.ones((dat.shape[0],dat.shape[1])), gamma2=0, low_f=0.1, aberr_cor=False, PD_f=deconv['PD_f'], straylight_corr=deconv['straylight_correction'])
             sly, slx = slice(5,sly.stop-sly.start+5), slice(5,slx.stop-slx.start+5)
         else:
             old_data = data[...,scan].copy()
